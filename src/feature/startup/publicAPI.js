a7116aee0ee733b3aa116a4f00d86fee7aea98e5
Kevin Bridges <kevin@wiiBridges.com>
2017-11-08 16:51:00 -0600

    startup/publicAPI.js no longer needs injectContext

--- src/feature/startup/publicAPI.js
+++ src/feature/startup/publicAPI.js
@@ -1,13 +1,12 @@
-import {injectContext}    from '../../util/feature-u';
 import {areFontsLoaded,
         isDeviceReady}    from './state';
 
 /**
  * The 'startup' feature public API, promoted through app.startup
  */
-export default injectContext( (feature, app) => ({
+export default {
   selectors: {
     areFontsLoaded,
     isDeviceReady,
   },
-}) );
+};
