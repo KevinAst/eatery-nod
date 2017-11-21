cf6c1b8dfa5d1fdc05ed8f063816b42871bcac54
Kevin Bridges <kevin@wiiBridges.com>
2017-11-21 17:18:17 -0600

    finalize 'leftNav' feature doc

---
+++ src/feature/leftNav/README.md
@@ -0,0 +1,9 @@
+# leftNav feature
+
+The **'leftNav'** feature introduces the app-specific Drawer/SideBar
+on the left side of our app.
+
+This feature is app-specific, as it has knowledge of the various items
+that make up the app ... coupled through feature cross-communication.
+In general, it merely activates other features of the app.
+
--- src/feature/leftNav/index.js
+++ src/feature/leftNav/index.js
@@ -3,13 +3,12 @@ import publicAPI        from './publicAPI';
 import appWillStart     from './appWillStart';
 
 /**
- * The 'leftNav' feature introduces the app-specific Drawer/SideBar 
- * on the left-hand side of our app.
+ * The **'leftNav'** feature introduces the app-specific Drawer/SideBar
+ * on the left side of our app.
  *
- * ?? This feature is app-specific, as it has knowledge of the various
- * features that make up our app.
- *
- * ?? In general, the leftNav menu activates various features of our app, and is therefore ??what-I-said-above
+ * This feature is app-specific, as it has knowledge of the various items
+ * that make up the app ... coupled through feature cross-communication.
+ * In general, it merely activates other features of the app.
  */
 export default createFeature({
   name: 'leftNav',
