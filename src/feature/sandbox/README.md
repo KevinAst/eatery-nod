a273ae69f8782619ff41db32c44c228a713db50d
Kevin Bridges <kevin@wiiBridges.com>
2017-12-03 10:58:41 -0600

    sandbox

--- src/feature/sandbox/README.md
+++ src/feature/sandbox/README.md
@@ -2,3 +2,10 @@
 
 The **'sandbox'** feature promotes a variety of interactive tests,
 used in development, that can easily be disabled.
+
+
+## Screen Flow
+
+When enabled, here are the sandbox options promoted in the leftNav screen ...
+
+![Screen Flow](docs/ScreenFlow.png)
--- src/feature/sandbox/index.js
+++ src/feature/sandbox/index.js
@@ -4,6 +4,11 @@ import publicAPI        from './publicAPI';
 /**
  * The **'sandbox'** feature promotes a variety of interactive tests,
  * used in development, that can easily be disabled.
+ *
+ *
+ * **Screen Flow**
+ *
+ * You can see a Screen Flow diagram at: `docs/ScreenFlow.png`.
  */
 export default createFeature({
   name:    'sandbox',
