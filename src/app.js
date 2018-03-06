5dfcbe47bbc5f276c33e25fbec378af05d1bdbed
Kevin Bridges <kevin@wiiBridges.com>
2018-03-06 06:29:28 -0600

    modularized feature-u Integration Tests (in eatery-nod)

--- src/app.js
+++ src/app.js
@@ -7,31 +7,73 @@ import {logicAspect}     from 'feature-redux-logic';
 import {routeAspect}     from 'feature-router';
 import SplashScreen      from './util/comp/SplashScreen';
 import features          from './feature'; // the set of features that comprise this application
-import logActions        from './feature/logActions'; // enable/disable eatery-nod's action logger
 
-// define our set of "plugable" feature-u Aspects, conforming to our app's run-time stack
-const aspects = [
-  reducerAspect, // redux          ... extending: Feature.reducer
-  logicAspect,   // redux-logic    ... extending: Feature.logic
-  routeAspect,   // Feature Routes ... extending: Feature.route
-];
+// following imports are for diagnostic purposes only (i.e. NON Production)
+import {diag}      from './util/diagnosticUtil';
+import logActions  from './feature/logActions'; // enable eatery-nod "logActions" feature
+import sandbox     from './feature/sandbox';    // enable eatery-nod "sandbox" feature (in left-nav)
+import feature_u_integrationTests  from './util/feature-u.integrationTests';
 
-// configure Aspects (as needed)
-// ... StateRouter fallback screen (when no routes are in effect)
-routeAspect.fallbackElm = <SplashScreen msg="I'm trying to think but it hurts!"/>;
-// ... StateRouter animation hook
-routeAspect.componentWillUpdateHook = () => LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-
-// configure logging
-launchApp.diag.logf.enable(); // feature-u/plugin diagnostic logging
-// launchApp.diag.logf.elm2html = (elm) => elm; // show react elms as object blobs
-logActions.enabled = false;   // eatery-nod's action logger
+configureDiagnostics();
 
 // launch our app, exposing the feature-u App object (facilitating cross-feature communication)!
 export default launchApp({
-  aspects,
+  aspects: appAspects(),
   features,
   registerRootAppElm(rootAppElm) {
     Expo.registerRootComponent(()=>rootAppElm); // convert rootAppElm to a React Component
   }
 });
+
+
+// accumulate/configure our Aspect plugins
+// ... matching our app's run-time stack
+function appAspects() {
+
+  // here is our run-time stack
+  const aspects = [
+    reducerAspect, // redux          ... extending: Feature.reducer
+    logicAspect,   // redux-logic    ... extending: Feature.logic
+    routeAspect,   // Feature Routes ... extending: Feature.route
+  ];
+
+  // configure Aspects (as needed)
+  // ... StateRouter fallback screen (when no routes are in effect)
+  routeAspect.config.fallbackElm$ = <SplashScreen msg="I'm trying to think but it hurts!"/>;
+  // ... StateRouter animation hook
+  routeAspect.config.componentWillUpdateHook$ = () => LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+
+  // beam me up Scotty :-)
+  return aspects;
+}
+
+
+// configure our diagnostics (if any)
+function configureDiagnostics() {
+
+  // --- eatery-nod logging/sandbox ... ------------------------------------------
+  diag.skip('enable eatery-nod "logActions" feature', () => {
+    logActions.enabled = true;
+  });
+  diag.skip('enable eatery-nod "sandbox" feature (in left-nav)', () => {
+    sandbox.enabled = true;
+  });
+
+  // --- feature-u logging related ... -------------------------------------------
+  diag.skip('enable feature-u logging', () => {
+    launchApp.diag.logf.enable();
+  });
+  diag.skip('show feature-u react elms as object blobs', () => {
+    launchApp.diag.logf.elm2html = (elm) => elm;
+  });
+  diag.skip('show feature-u react elms as html markup', () => {
+    // NOTE: requires ... import ReactDOMServer from 'react-dom/server';
+    //       UNTESTED: react-native / expo has issues resolving this in node
+    launchApp.diag.logf.elm2html = (elm) => ReactDOMServer.renderToStaticMarkup(elm);
+  });
+
+  // --- feature-u integration tests ... -----------------------------------------
+  diag.skip('perform feature-u integration tests', () => {
+    feature_u_integrationTests();
+  });
+}
